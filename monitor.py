1861cb26457cad557826f6260f2b0bf23ad2a9f3
smnzm <samanazimi.dm@gmail.com>
2025-04-12 14:17:36 +0330

    khodro45

--- monitor.py
+++ monitor.py
@@ -14,7 +14,7 @@ def read_keywords_from_excel(filename):
     return keywords
 
 # List of websites to monitor
-websites = ["https://bama.ir/"]
+websites = ["https://khodro45.com/"]
 
 # Function to get both ranks from a single API request
 def get_ranks(keyword, website, api_key):
